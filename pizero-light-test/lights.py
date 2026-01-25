6511ab6618105176ac09ceed3ecd6928e5016cb6
danielhood <danielhood@hotmail.com>
2026-01-24 20:56:51 -0500

    Force on at end of cycle

--- pizero-light-test/lights.py
+++ pizero-light-test/lights.py
@@ -43,11 +43,11 @@ try:
                 led5.off()
             sleep(0.1)
 
-        led1.off()
-        led2.off()
-        led3.off()
-        led4.off()
-        led5.off()
+        led1.on()
+        led2.on()
+        led3.on()
+        led4.on()
+        led5.on()
 
         sleep(1)
 
